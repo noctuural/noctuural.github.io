4fe1cd3e403d1e4b542983e1c3a55a914e656dee
noctuural <sylvesterwalt@gmail.com>
2026-01-03 23:11:53 +0800

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import ImgurGallery from "./ImgurGallery";
+
+function App() {
+  return (
+    <div>
+      <h1>My Imgur Album</h1>
+      <ImgurGallery albumHash="AbCdEfG" />
+    </div>
+  );
+}
+
+export default App;
